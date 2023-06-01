65d9a489ba6f7e6c3881493ec97b507399bce261
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2023-06-01 15:45:50 +0400

    refactor: remove self-check message

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,4 @@ import Game from './game.js';
 
 const game = new Game();
 
-game.start(); 
-
-console.log(
-  `
-  Внизу выводится правильный ответ для проверки.
-
-  Самоооценка - 270 баллов 
-   
-  `
-)
+game.start();
